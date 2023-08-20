060fa5107ae2064a2b2c9c008faa59d178eacb9d
Animesh404 <akjha6680@gmail.com>
2023-08-20 19:05:01 +0530

    forms, validation and animation

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -40,16 +40,21 @@
         
 //     )
 // }
-
+import { v4 as uuidv4} from 'uuid'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
-
+import FeedbackForm from "./components/FeedbackForm"
 
 const App = () => {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = newFeedback =>{
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback,...feedback])
+    }
 
     const deleteFeedback = id => {
         if(window.confirm('Are you Sure you want to Delete?')){
@@ -61,6 +66,7 @@ const App = () => {
         <>
         <Header />
         <div className="container">
+            <FeedbackForm handleAdd={addFeedback}/>
             <FeedbackStats feedback={feedback} />
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
